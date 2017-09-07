75a29467f8437e1a9519f84439d8766bbb4279b2
David Halewood <davidnhalewood@hotmail.com>
2017-09-07 15:43:38 +0100

    adding 12

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -21,8 +21,28 @@
 # f.to_f               # => 0.5
 
 class Fraction
+	attr_accessor :numerator, :denominator
+
+	def initialize(numerator, denominator)
+		self.numerator, self.denominator = numerator, denominator
+	end
+
+	def to_f
+		numerator / denominator.to_f
+	end
+
+	def to_s
+		"#{numerator}/#{denominator}"
+	end
+
   def gcd(a,b)
-    return a if b == 0
-    gcd(b, a%b)
-  end
+		return a if b == 0
+		gcd(b, a%b)
+  	end
+
+	def lowest
+		divisor = gcd(numerator, denominator)
+		Fraction.new((numerator/divisor), (denominator/divisor)) # very Meta
+	end
+	
 end
