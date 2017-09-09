87cd436e5c2dbffa4f330c1a1bb3e43064b3f42a
David Halewood <davidnhalewood@hotmail.com>
2017-09-09 08:23:07 +0100

    adding 2 and 3

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,13 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(num)
+	hash = {}
+	1.upto num do |x|
+		next if x % 2 == 0 # we only want odd number keys
+		@arr = Array.new(x) { |y| y + 1 } # creates a new array with ALL the numbers upto x
+		evens = @arr.select { |z| z % 2 == 0 } #select even numbers
+		hash[x] = evens # inserting x as the key and evens array as the value
+		end
+	hash
+end 
--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,11 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str)
+	hash = Hash.new { 0 }
+	str.split(' ').each do |x|
+		hash[x.downcase] += 1
+	end
+	hash
+end
